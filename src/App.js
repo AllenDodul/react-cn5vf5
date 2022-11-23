478deb4fdee99e45d61957b942fb920ddcf64244
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2022-11-23 19:10:46 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -27,6 +27,22 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
+      <input 
+        type="text" 
+        className="name"
+        placeholder="Name"
+      />
+      <input 
+        type="email" 
+        className="email"
+        placeholder="Email"
+      />
+      <input 
+        type="number" 
+        className="age"
+        placeholder="Age"
+      />
+      <button>Submit</button>
     </div>
   );
 }
